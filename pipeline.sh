bd01fc3a17bb27fc8d074f072ede3faaba2f04ab
Vladimir Tomic <vladimir.tomic@gmail.com>
2024-09-17 19:09:25 +0200

    Update notebooks and scripts

--- pipeline.sh
+++ pipeline.sh
@@ -1,30 +1,30 @@
-python /opt/repos/strat/scripts/strat_prepare.py \
---prefix "$PREFIX" \
---suffix "$SUFFIX" \
---motif "$MOTIF_PRIM" \
---tolerance "$TOLERANCE" \
---cores "$CORES" \
---input_path "/opt/data/$DATASET/fastq/$CALLER/" \
---output_path "/opt/data/$DATASET/output/$CALLER/" \
-1> /opt/data_out/workdir/$DATASET.$CALLER.strat_prepare.std.out \
-2> /opt/data_out/workdir/$DATASET.$CALLER.strat_prepare.std.err
+# python /opt/repos/strat/scripts/strat_prepare.py \
+# --prefix "$PREFIX" \
+# --suffix "$SUFFIX" \
+# --motif "$MOTIF_PRIM" \
+# --tolerance "$TOLERANCE" \
+# --cores "$CORES" \
+# --input_path "/opt/data/$DATASET/fastq/$CALLER/" \
+# --output_path "/opt/data/$DATASET/output/$CALLER/" \
+# 1> /opt/data_out/workdir/$DATASET.$CALLER.strat_prepare.std.out \
+# 2> /opt/data_out/workdir/$DATASET.$CALLER.strat_prepare.std.err
 
-cat /opt/data/$DATASET/output/$CALLER/*.ontarget.tsv 1> /opt/data_out/workdir/$DATASET.$CALLER.ontarget.tsv
+# cat /opt/data/$DATASET/output/$CALLER/*.ontarget.tsv 1> /opt/data_out/workdir/$DATASET.$CALLER.ontarget.tsv
 
-python /opt/repos/strat/scripts/strat_process.py \
---motif_prim "$MOTIF_PRIM" \
---motif_scnd "$MOTIF_SCND" \
---threshold '1' \
---input_path "/opt/data_out/workdir/$DATASET.$CALLER.ontarget.tsv" \
---output_path "/opt/data_out/workdir/" \
-1> /opt/data_out/workdir/$DATASET.$CALLER.strat_process.std.out \
-2> /opt/data_out/workdir/$DATASET.$CALLER.strat_process.std.err
-
-# python /opt/repos/strat/scripts/strat_process_kmers.py \
+# python /opt/repos/strat/scripts/strat_process.py \
 # --motif_prim "$MOTIF_PRIM" \
 # --motif_scnd "$MOTIF_SCND" \
 # --threshold '1' \
-# --input_path "/opt/data_out/kmers/$DATASET.$CALLER.kmers.tsv" \
-# --output_path "/opt/data_out/kmers_processed/" \
-# 1> /opt/data_out/kmers_processed/$DATASET.$CALLER.strat_process_kmers.std.out \
-# 2> /opt/data_out/kmers_processed/$DATASET.$CALLER.strat_process_kmers.std.err
+# --input_path "/opt/data_out/workdir/$DATASET.$CALLER.ontarget.tsv" \
+# --output_path "/opt/data_out/workdir/" \
+# 1> /opt/data_out/workdir/$DATASET.$CALLER.strat_process.std.out \
+# 2> /opt/data_out/workdir/$DATASET.$CALLER.strat_process.std.err
+
+python /opt/repos/strat/scripts/strat_process_kmers.py \
+--motif_prim "$MOTIF_PRIM" \
+--motif_scnd "$MOTIF_SCND" \
+--threshold '1' \
+--input_path "/opt/data_out/kmers/$DATASET.$CALLER.kmers.tsv" \
+--output_path "/opt/data_out/kmers_processed/" \
+1> /opt/data_out/kmers_processed/$DATASET.$CALLER.strat_process_kmers.std.out \
+2> /opt/data_out/kmers_processed/$DATASET.$CALLER.strat_process_kmers.std.err
